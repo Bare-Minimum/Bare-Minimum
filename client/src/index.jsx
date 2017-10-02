7a4acc3f9c093ab7e67de227740304200d652dba
Justin Kwok <jkwok0714@gmail.com>
2017-10-02 14:30:10 -0700

    Login will now request a user look-up from the database

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,16 +6,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	userSignup: ''
+    	userSignup: '',
+      userLogin: ''
     };
 
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleSignupChange = this.handleSignupChange.bind(this);
+    this.handleLoginChange = this.handleLoginChange.bind(this);
     this.signup = this.signup.bind(this);
   }
 
-  handleChange(event) {
+  handleSignupChange(event) {
   	this.setState({userSignup: event.target.value})
+  }
+
+  handleLoginChange(event) {
+    this.setState({userLogin: event.target.value});
   }
 
   signup() {
@@ -23,7 +29,7 @@ class App extends React.Component {
   	let user = {
   		name: this.state.userSignup
   	}
-  	
+
     $.ajax({
       url: 'http://127.0.0.1:3000/signup',
       method: 'POST',
@@ -39,18 +45,39 @@ class App extends React.Component {
     this.setState({userSignup: ''});
   }
 
+  login() {
+    let user = {
+      name: this.state.userLogin
+    }
+
+    $.ajax({
+      url: 'http://127.0.0.1:3000/login',
+      method: 'POST',
+      data: user,
+      success: function(body) {
+        console.log('GET was a success ', body);
+      },
+      error: function(err) {
+        window.alert('Error: ' + err.responseText);
+      	console.log('error with GET', err);
+      }
+    })
+
+    this.setState({userLogin: ''});
+  }
+
   render() {
     return (
       <div>
         <p>hello world from travel app component</p>
         <label>sign up for username </label>
-        <input type="text" value={this.state.userSignup} onChange={this.handleChange}></input>
+        <input type="text" value={this.state.userSignup} onChange={this.handleSignupChange}></input>
         <button onClick={() => this.signup()}>submit</button>
         <br></br>
         <br></br>
         <label>log in with username </label>
-        <input></input>
-        <button>submit</button>
+        <input type="text" value={this.state.userLogin} onChange={this.handleLoginChange} placeholder="Login name"></input>
+        <button onClick={() => this.login()}>submit</button>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const db = require('../database/index.js');
 const app = express();
-const query = require('../database/queryHelpers.js')
+const query = require('../database/queryHelpers.js');
 
 app.use(bodyParser.json());
 
@@ -15,16 +15,26 @@ app.use(express.static(__dirname + '/../client/dist'));
 //GET Handler
 
 
-
 //POST Handler
 app.post('/signup', (req, res) => {
   console.log(req.body);
   query.addUser(req.body, () => {
-    res.status(201).send('user submitted to DB')
-  })
-})
+    res.status(201).send('user submitted to DB');
+  });
+});
 
-
+app.post('/login', (req, res) => {
+  console.log(req.body.name);
+  query.findUser(req.body, (result) => {
+    if (result.length !== 0) {
+      // TODO: Handle cookies, sessions
+      console.log('User found, log in');
+      res.status(200).send('Log in matches!');
+    } else {
+      res.status(404).send('Bad user!');
+    }
+  });
+});
 
 
 app.listen(process.env.PORT, () => {
