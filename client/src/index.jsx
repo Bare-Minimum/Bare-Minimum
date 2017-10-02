d4d1831eee9730173cb26dea13994d75f58ffc85
Justin Kwok <jkwok0714@gmail.com>
2017-10-02 14:41:27 -0700

    Refactored code to remove duplicate functions

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,47 +11,25 @@ class App extends React.Component {
     };
 
 
-    this.handleSignupChange = this.handleSignupChange.bind(this);
-    this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.signup = this.signup.bind(this);
   }
 
-  handleSignupChange(event) {
-  	this.setState({userSignup: event.target.value})
+  handleChange(event, field) {
+    if (field === 'Signup') {
+      this.setState({userSignup: event.target.value})
+    } else if (field === 'Login') {
+      this.setState({userLogin: event.target.value})
+    }
   }
 
-  handleLoginChange(event) {
-    this.setState({userLogin: event.target.value});
-  }
-
-  signup() {
-
-  	let user = {
-  		name: this.state.userSignup
-  	}
-
-    $.ajax({
-      url: 'http://127.0.0.1:3000/signup',
-      method: 'POST',
-      data: user,
-      success: function(body) {
-        console.log('POST was a success ', body);
-      },
-      error: function(err) {
-      	console.log('error with POST', err)
-      }
-    })
-
-    this.setState({userSignup: ''});
-  }
-
-  login() {
+  handleSubmit(field) {
     let user = {
-      name: this.state.userLogin
+      name: this.state['user' + field]
     }
 
     $.ajax({
-      url: 'http://127.0.0.1:3000/login',
+      url: 'http://127.0.0.1:3000/' + field.toLowerCase(),
       method: 'POST',
       data: user,
       success: function(body) {
@@ -62,8 +40,11 @@ class App extends React.Component {
       	console.log('error with GET', err);
       }
     })
-
-    this.setState({userLogin: ''});
+    if (field === 'Login') {
+      this.setState({userLogin: ''});
+    } else if (field === 'Signup') {
+      this.setState({userSignup: ''});
+    }
   }
 
   render() {
@@ -71,13 +52,15 @@ class App extends React.Component {
       <div>
         <p>hello world from travel app component</p>
         <label>sign up for username </label>
-        <input type="text" value={this.state.userSignup} onChange={this.handleSignupChange}></input>
-        <button onClick={() => this.signup()}>submit</button>
+        <input type="text" value={this.state.userSignup}
+          onChange={(e) => this.handleChange(e, 'Signup')}></input>
+        <button onClick={() => this.handleSubmit('Signup')}>submit</button>
         <br></br>
         <br></br>
         <label>log in with username </label>
-        <input type="text" value={this.state.userLogin} onChange={this.handleLoginChange} placeholder="Login name"></input>
-        <button onClick={() => this.login()}>submit</button>
+        <input type="text" value={this.state.userLogin}
+          onChange={(e) => this.handleChange(e, 'Login')} placeholder="Login name"></input>
+        <button onClick={() => this.handleSubmit('Login')}>submit</button>
       </div>
     );
   }
