ce8ba91be73f677f0e922084b0a8db94d2b66064
Justin Kwok <jkwok0714@gmail.com>
2017-10-03 10:16:14 -0700

    Add second app infrastructure, methods for loading second app

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,20 @@ var SRC_DIR = __dirname + '/client/src';
 var DIST_DIR = __dirname + '/client/dist';
 
 module.exports = {
- entry: `${SRC_DIR}/index.jsx`,
- output: {
-   filename: 'bundle.js',
-   path: DIST_DIR
+  entry: {
+   main: `${SRC_DIR}/index.jsx`,
+   dashboard: `${SRC_DIR}/dashboard.jsx`
  },
+  output: {
+    filename: '[name].bundle.js',
+    path: DIST_DIR
+  },
  module : {
    loaders : [
      {
        test : /\.jsx?/,
        include : SRC_DIR,
-       loader : 'babel-loader',      
+       loader : 'babel-loader',
        query: {
          presets: ['react', 'es2015']
       }
