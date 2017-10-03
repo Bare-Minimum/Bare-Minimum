ad6bce7561dafb2879c747ecd4d1c6bd7bcca9b7
laraismael1 <laraismael123@gmail.com>
2017-10-03 10:36:08 -0700

    update after conflicts

---
+++ client/src/components/tripDashboard/tripDashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TripDashboard extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return(
+			<div>  
+        <ul>
+          
+        </ul>
+      </div>
+		)
+	}
+}
+
+export default TripDashboard;
