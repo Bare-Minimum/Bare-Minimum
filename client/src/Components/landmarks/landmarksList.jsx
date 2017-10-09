d287eb44806f1950901a74756d0f36adcaf20b67
Justin Kwok <jkwok0714@gmail.com>
2017-10-09 10:29:45 -0700

    cleanup of Expenses and landmarks

---
+++ client/src/Components/landmarks/landmarksList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LandmarksList = (props) => {
+  return (
+    <div>
+      <Table>
+        <tbody>
+        <tr>
+          <th></th>
+          <th> Description </th>
+          <th> URL </th>
+          <th> Address </th>
+          <th> Suggested by </th>
+          <th> Votes </th>
+        </tr>
+        { props.landmarks.map(landmark => <LandmarkEntry user={props.user} fetch={props.fetch} landmark={landmark} key={landmark.id}/>) }
+
+        </tbody>
+      </Table>
+		</div>
+	);
+};
+
+export default LandmarksList;
--- client/src/components/landmarks/landmarks.jsx
+++ client/src/components/landmarks/landmarks.jsx
@@ -7,14 +7,13 @@ import { Table } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 
-import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from '../../Reducers';
-const store = createStore(reducer.travelReducer);
-const { getState } = store;
 import { connect } from 'react-redux';
 import TripNavBar from '../tripDashboard/tripNavBar.jsx';
 import dummyData from '../tripDashboard/dummyData.js';
+import LandmarkEntry from './landmarkEntry.jsx';
+import LandmarksList from './LandmarksList';
 
 const SERVER_URL = HOSTNAME;
 
@@ -24,8 +23,6 @@ class Landmarks extends React.Component {
 		this.state = {
 			landmarks: []
 		}
-		//Listen to changes in the redux store
-		//store.subscribe(() => {this.setState({reload:false})});
 		this.fetch = this.fetch.bind(this);
 	}
 	fetch() {
@@ -37,7 +34,6 @@ class Landmarks extends React.Component {
         tripId: this.props.trip.id
       },
   		success: function(body) {
-        console.log('coming back from server,', body);
         context.setState({landmarks: body})
   		},
   		error: function(err) {
@@ -63,8 +59,6 @@ class Landmarks extends React.Component {
 	}
 }
 
-
-
 /*
 Child components for Landmarks view =======================================================
 */
@@ -72,7 +66,6 @@ Child components for Landmarks view ============================================
 class LandmarkSubmit extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
       description: '',
       address: '',
@@ -80,7 +73,6 @@ class LandmarkSubmit extends React.Component {
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.submit = this.submit.bind(this);
-
 	}
   handleChange(e, field) {
   	if (field === 'description') {
@@ -110,7 +102,6 @@ class LandmarkSubmit extends React.Component {
       },
       error: function(err) {
         window.alert('Error: ' + err.responseText);
-      	console.log('error with GET', err);
       }
     })
     this.setState({
@@ -146,72 +137,8 @@ class LandmarkSubmit extends React.Component {
   }
 };
 
-const LandmarksList = (props) => {
-  
-	function handleClick() {
-
-  }
-  return (
-    <div>
-      <Table>
-        <tbody>
-        <tr>
-          <th></th>
-          <th> Description </th>
-          <th> URL </th>
-          <th> Address </th>
-          <th> Suggested by </th>
-          <th> Votes </th>
-        </tr>
-        { props.landmarks.map(landmark => <LandmarkEntry user={props.user} fetch={props.fetch} landmark={landmark} key={landmark.id}/>) }
-        
-        </tbody>
-      </Table> 
-		</div>
-	);
-};
-
-const LandmarkEntry = (props) => {
-  function handleClick() {
-    const context = this;
-    let obj = {
-      landmarkId: props.landmark.id,
-      userId: props.user.id
-    };
-    $.ajax({
-      url: SERVER_URL + '/vote',
-      method: 'POST',
-      data: obj,
-      success: function(body) {
-        props.fetch();
-      }
-    })
-	}
-
-  const buttonState = () => {
-    let showButton = true;
-    console.log(props.landmark.User.id)
-    for (let vote in props.landmark.votes) {
-      if (props.user.id === props.landmark.votes[vote].userId) {
-        showButton = false;
-      }
-    }
-    return showButton;
-  }
-	return (
-    <tr>
-      <td><Button style={buttonState() ? {} : { display: 'none' }} onClick={handleClick}>vote</Button></td>
-      <td>{props.landmark.description}</td>
-      <td><a href={props.landmark.url}>{props.landmark.url}</a></td>
-      <td>{props.landmark.address}</td>
-      <td>{props.landmark.User.name}</td>
-      <td>{props.landmark.votes.length}</td>
-    </tr> 
-	);
-};
-
 let mapStateToProps = ({ user, trip }) => {
   return ({ user, trip })
-}
+};
 
-export default connect(mapStateToProps)(Landmarks)
+export default connect(mapStateToProps)(Landmarks);
